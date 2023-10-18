daba57c360c5cc69741fa35b6da075250adc8447
e1sndev <dev.e1sn@gmail.com>
2023-10-18 18:01:49 +0400

    Update log.service.ts

--- src/log/log.service.ts
+++ src/log/log.service.ts
@@ -21,17 +21,7 @@ export class LogService {
     const todayMinus30days = new Date();
     todayMinus30days.setDate(todayMinus30days.getDate() - 30);
     todayMinus30days.setHours(0, 0, 0);
-    const data = await this.logRepository
-      .createQueryBuilder('log')
-      .where('log.createdAt >= :todayMinus30days', {
-        todayMinus30days: todayMinus30days.toISOString(),
-      })
-      .where('log.createdAt <= :today', {
-        today: today.toISOString(),
-      })
-      .orderBy('log.createdAt', 'DESC')
-      .groupBy('log.createdAt, log.id')
-      .getMany();
+    const data = await this.logRepository.createQueryBuilder('log').getMany();
     console.log(data);
   }
 }
